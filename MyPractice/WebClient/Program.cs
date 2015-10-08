2e25a6a1a615daeeef1451d9a72ca965e76738cc
Denys Pokatilov <kazantip4ik87@gmail.com>
2015-10-08 23:12:43 +0300

    first commit

---
+++ MyPractice/WebClient/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPractice
+{
+
+    public class WebClient
+    {
+        static void Main(string[] args)
+        {
+            new WebClient().Run(args);
+        }
+
+        public void Run(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Enter any url adress, like this type http://www.vk.com");
+            }
+            else
+            {
+                Console.ReadLine();
+                foreach (var arg in args)
+                {
+                    if (arg == "-url")
+                    {
+                       
+                        using (Stream responceStream = WebRequest.Create(arg).GetResponse().GetResponseStream())
+
+                        using (StreamReader responceReader = new StreamReader(responceStream))
+                        {
+                            Console.WriteLine("It works!");
+                            while (responceReader.Peek() >= 0)
+                            {
+                                Console.WriteLine(responceReader.ReadLine());
+                            }
+                        }
+                    }
+
+                    Console.ReadKey();
+                }
+
+            }
+
+
+        }
+    }
+}
+
+
+
