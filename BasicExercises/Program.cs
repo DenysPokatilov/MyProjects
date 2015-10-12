e9e12cca55f7f2a1ee1e458ee94c8f315deeeddf
Artyom Yakovenko <artyom.yakovenko@gmail.com>
2015-10-13 01:17:16 +0300

    Added a new console project

---
+++ BasicExercises/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BasicExercises
+{
+
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            var program = new Program();
+
+            program.DisplayTasksMenu();
+        }
+
+        public void DisplayTasksMenu()
+        {
+            Console.WriteLine("Введи номер задачи из списка или q для выхода:");
+            Console.WriteLine("");
+            Console.WriteLine("1. Факториал.");
+            Console.WriteLine("2. Ряд Фибоначчи.");
+            Console.WriteLine("3. Имя месяца по его номеру на указанном языке.");
+            Console.WriteLine("4. Перевернуть строку наоборот.");
+            Console.WriteLine("5. Посчитать количество слов в строке.");
+            Console.WriteLine("6. Поиск слова в строке.");
+            Console.WriteLine("7. Статистика гласных в строке.");
+            Console.WriteLine("8. Шифр Цезаря.");
+            Console.WriteLine("");
+        }
+
+    }
+
+}
