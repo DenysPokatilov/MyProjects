459467c023abf1ff5aa8b394e9e37ff1224c1075
Denys Pokatilov <kazantip4ik87@gmail.com>
2015-10-28 13:32:06 +0200

    Complate the third mothod, but it to much Juniors cod ;-)

--- second Practice/SecondPractice/SecondPractice/Program.cs
+++ second Practice/SecondPractice/SecondPractice/Program.cs
@@ -50,8 +50,9 @@ namespace BasicExercises
                           }
                     }
                     catch (Exception ex) { Console.WriteLine(ex.Message); }
-                }       
-                Console.ReadLine();
+                }
+                DisplayTasksMenu();
+               
             } while (line.Length != 0);
         }
 
@@ -114,36 +115,135 @@ namespace BasicExercises
                  {
                      case "1":
                          Console.WriteLine("Выберите номер месяца");
-                         string number = Console.ReadLine();
-                         //  var montH = new OrderedDictionary{ 
-                         //  { "1","Январь"},
-                         //  {"2","Февраль"}
-                         // };
+                         var inputRus = Convert.ToInt32(Console.ReadLine());              
+                         var elementRus = new Dictionary<int, string>();
+                         elementRus.Add(1, "Январь");elementRus.Add(2, "Февраль");
+                         elementRus.Add(3, "Март");elementRus.Add(4, "Апрель");
+                         elementRus.Add(5, "Май");elementRus.Add(6, "Июнь");
+                         elementRus.Add(7, "Июль");elementRus.Add(8, "Август");
+                         elementRus.Add(9, "Сентябрь");elementRus.Add(10, "Октябрь");
+                         elementRus.Add(11, "Ноябрь");elementRus.Add(12, "Декабрь");
 
-                         //foreach (string number in montH)
-                         //   {
-                         //       Console.WriteLine("{0}"number);
-                         //   } 
-                         //  {
-                         //      Console.WriteLine(number.);
-                         //  }                                                      
-
-
+                         if (inputRus == null)
+                         {
+                             Console.WriteLine("вы не ввели элемент");
+                         }
+                         else
+                         {
+                             foreach (KeyValuePair<int, string> item in elementRus)
+                             {   if (inputRus == item.Key)
+                                 {
+                                     Console.WriteLine("{0}", item);
+                                 }
+                             }
+                         }  
                          break;
                      case "2":
                          Console.WriteLine("Choose the number of month");
+                          var inputEng = Convert.ToInt32(Console.ReadLine());              
+                         var elementEng = new Dictionary<int, string>();
+                         elementEng.Add(1, "January");elementEng.Add(2, "Februaury");
+                         elementEng.Add(3, "March");elementEng.Add(4, "April");
+                         elementEng.Add(5, "May");elementEng.Add(6, "june");
+                         elementEng.Add(7, "July");elementEng.Add(8, "Augest");
+                         elementEng.Add(9, "September");elementEng.Add(10, "October");
+                         elementEng.Add(11, "November");elementEng.Add(12, "December");
+
+                         if (inputEng == null)
+                         {
+                             Console.WriteLine("you did'not enter the number of month");
+                         }
+
+                         else
+                         {
+                             foreach (KeyValuePair<int, string> item in elementEng)
+                             {
+                                 if (inputEng == item.Key)
+                                 {
+                                     Console.WriteLine("{0}", item);
+                                 }
+                             }
+                         }  
                          break;
                      case "3":
                          Console.WriteLine("Sélectionnez le mois");
+                          var inputFr = Convert.ToInt32(Console.ReadLine());              
+                         var elementFr = new Dictionary<int, string>();
+                         elementFr.Add(1, "Janvier");elementFr.Add(2, "Fevrier");
+                         elementFr.Add(3, "Mars");elementFr.Add(4, "Avril");
+                         elementFr.Add(5, "Mai");elementFr.Add(6, "Juin");
+                         elementFr.Add(7, "Juillet");elementFr.Add(8, "Aout");
+                         elementFr.Add(9, "Septembre");elementFr.Add(10, "Octobre");
+                         elementFr.Add(11, "Novembre");elementFr.Add(12, "Decembre");
+
+                         if (inputFr == null)
+                         {
+                             Console.WriteLine("Vous no avez pas entré numero le mois");
+                         }
+                         else
+                         {  foreach (KeyValuePair<int, string> item in elementFr)
+                             {
+                                 if (inputFr == item.Key)
+                                 {
+                                     Console.WriteLine("{0}", item);
+                                 }
+                             }
+                         }  
                          break;
                      case "4":
                          Console.WriteLine("Виберіть номер місяця");
+                           var inputUkr = Convert.ToInt32(Console.ReadLine());
+                           var elementUkr = new Dictionary<int, string>();
+                         elementUkr.Add(1, "Сiчень");elementUkr.Add(2, "Лютий");
+                         elementUkr.Add(3, "Березень");elementUkr.Add(4, "Квiтень");
+                         elementUkr.Add(5, "Травень");elementUkr.Add(6, "Червень");
+                         elementUkr.Add(7, "Липень");elementUkr.Add(8, "Серпень");
+                         elementUkr.Add(9, "Вересень");elementUkr.Add(10, "Жовтень");
+                         elementUkr.Add(11, "Листопад");elementUkr.Add(12, "Грудень");
+
+                         if (inputUkr == null)
+                         {
+                             Console.WriteLine("Ви не зробили введеня номера мiсяця");
+                         }
+                         else
+                         {
+                             foreach (KeyValuePair<int, string> item in elementUkr)
+                             {
+                                 if (inputUkr == item.Key)
+                                 {
+                                     Console.WriteLine("{0}", item);
+                                 }
+                             }
+                         }
                          break;
                      case "5":
                          Console.WriteLine("Wybierz miesiąc");
+                          var inputPl = Convert.ToInt32(Console.ReadLine());
+                          var elementPl = new Dictionary<int, string>();
+                         elementPl.Add(1, "tyczeń");elementPl.Add(2, "luty");
+                         elementPl.Add(3, "marzec ");elementPl.Add(4, "kwiecień ");
+                         elementPl.Add(5, "maj");elementPl.Add(6, "czerwiec");
+                         elementPl.Add(7, "lipiec");elementPl.Add(8, "seirpien");
+                         elementPl.Add(9, "wrzesień");elementPl.Add(10, "wrzesień");
+                         elementPl.Add(11, "listopad ");elementPl.Add(12, "listopad ");
+
+                         if (inputPl == null)
+                         {
+                             Console.WriteLine("Nie wpisałeś miesiąc");
+                         }
+                         else
+                         {
+                             foreach (KeyValuePair<int, string> item in elementPl)
+                             {
+                                 if (inputPl == item.Key)
+                                 {
+                                     Console.WriteLine("{0}", item);
+                                 }
+                             }
+                         }
                          break;
 
-                     default: Console.WriteLine("Не правельный ввод");
+                     default: Console.WriteLine("input is not correct");
                          break;
                  }
              }
@@ -279,4 +379,3 @@ namespace BasicExercises
 }
 
 
-//  string input = args.Length == 0 ? null : args[0];
